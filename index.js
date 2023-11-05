a3ac230a6e0ad3d860d56f47d77d2360a50ab747
suhyeon lee <LeeeeSuHyeon@users.noreply.github.com>
2023-11-05 13:50:05 +0000

    로그인 기능 구현 및 로그인에 따른 메뉴바 변경

--- index.js
+++ index.js
@@ -3,8 +3,28 @@ const app = express();
 app.set('views', __dirname +'/views');
 app.set('view engine', 'ejs');
 
-// 포트 번호를 환경 변수로 설정하고, 기본값은 7070로 지정
-const port = process.env.PORT || 7070;
+// 포트 번호를 환경 변수로 설정하고, 기본값은 7080로 지정
+const port = process.env.PORT || 7080;
+
+// session 관련 모듈, 세션 DB 저장 모듈 
+var session = require('express-session'); 
+var MySqlStore = require('express-mysql-session')(session); // db에 저장하기 위한 세션 
+
+var options = {
+   host : '172.18.0.2', // MySQL 호스트 주소로 컨테이너를 실행하는 호스트 머신의 IP 주소를 사용
+   user : 'project-db',
+   password : '1234',
+   database : 'project-db'
+   };
+
+var sessionStore = new MySqlStore(options);
+   
+app.use(session({
+   secret : 'keyboard cat',
+   resave : false,
+   saveUninitialized : true,
+   store : sessionStore
+}));
 
 
 // body-parser 모듈 
@@ -14,9 +34,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 // 사용자 정의 모듈 
 var rootRouter = require('./router/rootRouter');
+var authRouter = require('./router/authRouter');
 
 // 라우팅 
 app.use('/', rootRouter);
+app.use('/auth', authRouter)
 
 
 //정적 파일 폴더 지정
